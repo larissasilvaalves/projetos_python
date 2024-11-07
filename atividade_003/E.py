2bf9acadde0f7f6718c50189b2d16cdcaeffae79
larissasilvaalves <larissaalvessilva180@gmail.com>
2024-11-07 16:38:01 -0300

    strings

--- atividade_003/E.py
+++ atividade_003/E.py
@@ -3,7 +3,7 @@ import random
 os.system('cls')
 
 #entrada
-numero_aleatorio = random.randint(1,1000000)
+numero_aleatorio = random.randint(3)
 
 print(f' voces acham que quanto em??'-'?')
 print(f'quais as chances de darmos certos? {numero_aleatorio}')
