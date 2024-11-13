5053a6d0ac3ba3e4b7c1c04b900bcb55decb85bc
larissasilvaalves <larissaalvessilva180@gamil.com>
2024-11-13 16:39:03 -0300

    atividades 6

---
+++ atividade_005/d.py
@@ -0,0 +1,15 @@
+import os 
+
+os.system('cls')
+
+print('-'*70)
+print('Contagem de números pares')
+print()
+
+for i in range(1, 100):
+    if i % 2  == 0:
+       
+       print(f'pares: {i}')
+
+print('-'*70)
+print('fim do seu programa')
---
+++ atividade_005/e.py
@@ -0,0 +1,12 @@
+import os
+import math
+os.system('cls')
+
+print('-'*70)
+print('A soma dos números pares')
+
+
+for i in range(1, 100):
+    if i % 2  == 0:
+       soma += i
+            
