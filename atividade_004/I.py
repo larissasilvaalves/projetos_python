b352776356ee4b3f43ba9744b3183af047952fec
larissasilvaalves <larissaalvessilva180@gamil.com>
2024-11-25 16:44:05 -0300

    atividade 007

--- atividade_004/I.py
+++ atividade_004/I.py
@@ -7,11 +7,11 @@ nome = str(input('entre com o seu nome completo: '))
 
 #processamento
 nome_quebrado = nome.split()
-prmeiro_nome = nome_quebrado[0]
+primeiro_nome = nome_quebrado[0]
 ultimo_nome = nome_quebrado[-1]
 
 #saída
 print(f'{nome}')
 print(f'{nome_quebrado}')
-print(f'o primeiro nome é: {prmeiro_nome}')
+print(f'o primeiro nome é: {primeiro_nome}')
 print(f'o ultimo nome é: {ultimo_nome}')
---
+++ atividade_006/B.py
@@ -0,0 +1,8 @@
+import os 
+
+os.system('cls')
+
+
+lista_aleatorio = [1, 2, 3, 4, 5, 6, 7]
+
+lista_aleatorio
